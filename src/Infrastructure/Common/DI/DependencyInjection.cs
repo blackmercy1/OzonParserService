39f75afa8b9b3dc0ee749ed006a0ef769cd8ec12
black_mercy <leshaar14@gmail.com>
2025-04-10 17:56:08 +0200

    feature: changed masstransit startup config

--- src/Infrastructure/Common/DI/DependencyInjection.cs
+++ src/Infrastructure/Common/DI/DependencyInjection.cs
@@ -1,17 +1,23 @@
 ﻿using Hangfire;
 using Hangfire.PostgreSql;
 using MassTransit;
+using ProductDataContract;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
+using OzonParserService.Application.Common.DateTimeProvider;
 using OzonParserService.Application.ParsingTasks.Jobs;
 using OzonParserService.Application.ParsingTasks.Persistance;
 using OzonParserService.Application.Publish;
+
+using OzonParserService.Infrastructure.Common.DateTime;
 using OzonParserService.Infrastructure.ParsingTaskPersistence;
 using OzonParserService.Infrastructure.ParsingTaskPersistence.Jobs;
 using OzonParserService.Infrastructure.Persistance;
+
 using OzonParserService.Infrastructure.ProductDataPersistance.Publisher;
 
 namespace OzonParserService.Infrastructure.Common.DI;
@@ -22,34 +28,34 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddPersistance(configuration);
+        services.AddPersistence(configuration);
         return services;
     }
 
-    private static IServiceCollection AddPersistance(
-        this IServiceCollection services,
+    private static IServiceCollection AddPersistence(this IServiceCollection services,
         IConfiguration configuration)
     {
         services
             .AddMassTransitServices(configuration)
             .AddDbContext<OzonDbContext>(options =>
-        {
-            options.UseNpgsql(configuration["ConnectionStrings:DefaultConnection"]);
-        });
+            {
+                options.UseNpgsql(configuration["ConnectionStrings:DefaultConnection"]);
+            });
 
         services
             .AddScoped<IParsingTaskRepository, ParsingTaskRepository>()
             .AddScoped<IProductDataPublisher, ProductDataPublisher>()
-            .AddScoped<IJob, ParsingTaskJob>();
-        
+            .AddScoped<IJob, ParsingTaskJob>()
+            .AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
         services.AddHangfire(cfg =>
-            cfg.UsePostgreSqlStorage(options 
+            cfg.UsePostgreSqlStorage(options
                 => options.UseNpgsqlConnection(configuration["ConnectionStrings:HangfireConnection"])));
         services.AddHangfireServer();
-        
+
         return services;
     }
-    
+
     private static IServiceCollection AddMassTransitServices(
         this IServiceCollection services,
         IConfiguration configuration)
@@ -57,23 +63,42 @@ public static class DependencyInjection
         services.AddMassTransit(busConfiguration =>
         {
             busConfiguration.SetKebabCaseEndpointNameFormatter();
+
             busConfiguration.UsingRabbitMq((context, cfg) =>
             {
-                cfg.Host(new Uri(configuration["MessageBroker:Host"]!), c =>
+                cfg.Host(
+                    new Uri(configuration["MessageBroker:Host"]!),
+                    c =>
+                    {
+                        c.Username(configuration["MessageBroker:UserName"]!);
+                        c.Password(configuration["MessageBroker:Password"]!);
+                    });
+
+                cfg.UseMessageRetry(r =>
                 {
-                    c.Username(configuration["MessageBroker:UserName"]!);
-                    c.Password(configuration["MessageBroker:Password"]!);
+                    r.Exponential(
+                        retryLimit: 4,
+                        minInterval: TimeSpan.FromSeconds(1),
+                        maxInterval: TimeSpan.FromSeconds(30),
+                        intervalDelta: TimeSpan.FromSeconds(2));
+                    r.Ignore<SecurityTokenExpiredException>();
+                    r.Ignore<SecurityTokenException>();
                 });
 
-                cfg.UseMessageRetry(retryConfigurator =>
+                cfg.UseCircuitBreaker(cb =>
                 {
-                    retryConfigurator.Interval(5, TimeSpan.FromSeconds(2));
+                    cb.TrackingPeriod = TimeSpan.FromMinutes(1);
+                    cb.TripThreshold = 15;
+                    cb.ActiveThreshold = 10;
+                    cb.ResetInterval = TimeSpan.FromMinutes(5);
                 });
-                
+
+                cfg.Publish<ProductDataMessage>();
+
                 cfg.ConfigureEndpoints(context);
             });
         });
-        
+
         return services;
     }
 }
