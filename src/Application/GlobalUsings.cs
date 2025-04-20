5d18e1e81ffcefeda5d206a127cf547b879da820
black_mercy <leshaar14@gmail.com>
2025-04-20 15:34:31 +0200

    feature: added global to application layer

--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -1,1 +1,13 @@
-﻿
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using FluentValidation;
+global using JetBrains.Annotations;
+global using Microsoft.Extensions.DependencyInjection;
+global using PuppeteerSharp;
+
+global using ErrorOr;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
