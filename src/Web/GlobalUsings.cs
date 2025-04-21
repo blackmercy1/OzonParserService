15859f1dde2bd0be0971b7e1b2124bacfa135ba5
black_mercy <leshaar14@gmail.com>
2025-04-21 09:13:49 +0200

    feature: added global using's to presentation layer

---
+++ src/Web/GlobalUsings.cs
@@ -0,0 +1,28 @@
+global using OzonParserService.Application;
+global using OzonParserService.Infrastructure.Common.DI;
+global using OzonParserService.Web.Common.DI;
+global using OzonParserService.Web.Configuration;
+
+global using OzonParserService.Web.Http;
+global using OzonParserService.Web.Common.Controllers;
+global using OzonParserService.Web.Common.Errors;
+
+global using Microsoft.Extensions.Options;
+global using System.Diagnostics;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.ModelBinding;
+global using Microsoft.AspNetCore.Mvc.Infrastructure;
+global using Microsoft.OpenApi.Models;
+
+global using Serilog;
+global using Hangfire;
+global using ErrorOr;
+global using AutoMapper;
+
+global using MediatR;
+global using FluentValidation;
+global using FluentValidation.AspNetCore;
+global using JetBrains.Annotations;
+
+
