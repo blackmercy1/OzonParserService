061ba7ba5c8a5f4e4f077fa9f292f816faa89a6d
black_mercy <leshaar14@gmail.com>
2025-04-21 08:57:04 +0200

    feature: added global using to infrastructure layer

--- src/Infrastructure/GlobalUsings.cs
+++ src/Infrastructure/GlobalUsings.cs
@@ -1,1 +1,26 @@
-﻿
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.Tokens;
+
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using OzonParserService.Infrastructure.Persistence;
+global using OzonParserService.Application.Publish;
+global using OzonParserService.Application.Common.Authentication;
+global using OzonParserService.Application.Common.DateTimeProvider;
+
+global using OzonParserService.Infrastructure.Common.Authentication;
+global using OzonParserService.Infrastructure.Common.DateTime;
+
+global using ProductDataContract;
+global using System.Text;
+global using System;
+global using MassTransit;
+
+global using Hangfire;
+global using Hangfire.PostgreSql;
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
