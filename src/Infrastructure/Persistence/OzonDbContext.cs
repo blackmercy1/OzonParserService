3dff1285aab72e0325eabfa32de72b0509f24f83
black_mercy <leshaar14@gmail.com>
2025-04-26 10:00:41 +0200

    feature: added interceptor to OzonContext

--- src/Infrastructure/Persistence/OzonDbContext.cs
+++ src/Infrastructure/Persistence/OzonDbContext.cs
@@ -1,16 +1,29 @@
 using OzonParserService.Domain.ParserTaskAggregate;
+using OzonParserService.Infrastructure.Persistence.Interceptors;
 
 namespace OzonParserService.Infrastructure.Persistence;
 
-public class OzonDbContext(DbContextOptions<OzonDbContext> options) : DbContext(options)
+public class OzonDbContext(
+    PublishDomainEventsInterceptor publishDomainEventsInterceptor,
+    DbContextOptions<OzonDbContext> options) : DbContext(options)
 {
     public DbSet<ParsingTask> ParsingTasks { get; set; }
 
     protected override void OnModelCreating(
         ModelBuilder modelBuilder)
     {
+        modelBuilder
+            .Ignore<List<IDomainEvent>>()
+            .ApplyConfigurationsFromAssembly(typeof(OzonDbContext).Assembly);
+        
         base.OnModelCreating(modelBuilder);
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .AddInterceptors(publishDomainEventsInterceptor);
 
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(OzonDbContext).Assembly);
+        base.OnConfiguring(optionsBuilder);
     }
 }
