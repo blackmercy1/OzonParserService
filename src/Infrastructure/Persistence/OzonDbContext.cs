1883dd14104e7c92246ad394030c5336b7d3084a
black_mercy <leshaar14@gmail.com>
2025-04-29 17:24:29 +0200

    feature: added Outbox message to application context

--- src/Infrastructure/Persistence/OzonDbContext.cs
+++ src/Infrastructure/Persistence/OzonDbContext.cs
@@ -1,13 +1,15 @@
 using OzonParserService.Domain.ParserTaskAggregate;
+using OzonParserService.Infrastructure.Outbox;
 using OzonParserService.Infrastructure.Persistence.Interceptors;
 
 namespace OzonParserService.Infrastructure.Persistence;
 
 public class OzonDbContext(
-    PublishDomainEventsInterceptor publishDomainEventsInterceptor,
+    PublishOutboxMessagesInterceptor publishOutboxMessagesInterceptor,
     DbContextOptions<OzonDbContext> options) : DbContext(options)
 {
     public DbSet<ParsingTask> ParsingTasks { get; set; }
+    public DbSet<OutboxMessage> OutboxMessages { get; set; }
 
     protected override void OnModelCreating(
         ModelBuilder modelBuilder)
@@ -23,7 +25,7 @@ public class OzonDbContext(
     {
         optionsBuilder
             .UseSnakeCaseNamingConvention()
-            .AddInterceptors(publishDomainEventsInterceptor);
+            .AddInterceptors(publishOutboxMessagesInterceptor);
 
         base.OnConfiguring(optionsBuilder);
     }
